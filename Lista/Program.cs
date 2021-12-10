2750ac684e101aa5c32942651a47f7707cfcbcc3
alan04003 <89245292+alan04003@users.noreply.github.com>
2021-12-10 13:52:14 +0100

    Add files via upload

---
+++ Lista/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+
+static int Minsta(List<int> listan) {
+        int min = listan[0];
+
+        foreach(int i in listan){
+            if(i < min)
+            {
+                min = i;
+                }
+            }
+
+            return min;
+        }
+
+static int Största(List<int> listan) {
+            int max = listan[0];
+            foreach(int i in listan){
+                if(i > max){
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+static void SkrivUt(List<int> listan) {
+        foreach(int i in listan){
+            Console.WriteLine(i);
+        }
+    }
+
+
+public static void Main (string[] args) {
+List<int> minLista = new List<int>();
+minLista.Add(11); minLista.Add(5);
+minLista.Add(12); minLista.Add(3);
+minLista.Add(6); minLista.Add(3);
+
+SkrivUt(minLista);
+
+Console.WriteLine("Största tal: "+Största(minLista));
+Console.WriteLine("Minsta tal: "+Minsta(minLista));
+
+}
+}
